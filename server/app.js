7ed6fcfbb690a6aa8d28c3795be8e66fecd823b8
“maulanaFandi” <maulana27fandi@gmail.com>
2024-02-16 16:18:59 +0700

    Fixing All Server Feature

---
+++ server/app.js
@@ -0,0 +1,17 @@
+if (process.argv.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const express = require("express");
+const app = express();
+const router = require("./routers");
+const errHandle = require("./middlewares/errorHandler");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(router);
+app.use(errHandle);
+
+module.exports = app;
---
+++ server/middlewares/errorHandler.js
@@ -0,0 +1,33 @@
+const errHandle = (err, req, res, next) => {
+    switch (err.name) {
+      case "SequelizeValidationError":
+      case "SequelizeUniqueConstraintError":
+        res.status(400).json({ message: err.errors[0].message });
+        break;
+      case "EmailEmpty":
+        res.status(400).json({ message: "Email Is Required" });
+        break;
+      case "PasswordEmpty":
+        res.status(400).json({ message: "Password Is Required" });
+        break;
+      case "InvalidLogin":
+        res.status(401).json({ message: "Wrong Email/Password" });
+        break;
+      case "NotFound":
+        res.status(400).json({ message: "Data Not Found" });
+        break;
+      case "Unauthorized":
+      case "JsonWebTokenError":
+        res.status(401).json({ message: "Invalid Token" });
+        break;
+      case "Forbiden":
+        res.status(403).json({ message: " Restricted Admin Only" });
+        break;
+  
+      default:
+        console.log(err);
+        res.status(500).json({ message: "Internal Server Error" });
+        break;
+    }
+  };
+  module.exports = errHandle;
